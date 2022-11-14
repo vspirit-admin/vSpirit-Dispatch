008345efc575f349e1826e7535911401cfd00e73
Evan <evan@dotevan.com>
2022-11-14 11:13:44 -0800

    More separation of development from production environments (#10)
    * if NODE_ENV not in production overwrite to DISPATCH_CALLSIGN + 'OUT'
    * changes CALLSIGN to DISPATCH_CALLSIGN
    * throws an error when an invalid HOPPIE_LOGON is supplied in the Hoppie request
    * consolidates log configuration
    * adds LOG_LEVEL env variable to set minimum log level
    * dev nodemon now also monitors .env

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import { log } from './log'
 import { arrivalMessage } from './arrivalMessage'
 import { schedule } from 'node-cron'
 
@@ -10,15 +11,26 @@ const checkEnv = function(envs: string[]) {
   }
 }
 
-checkEnv(['CALLSIGN']);
+for (const env of [
+  'NODE_ENV',
+  'DEV_MODE',
+  'LOG_LEVEL',
+  'DISPATCH_CALLSIGN',
+]) {
+  log.debug(env, process.env[env]);
+};
+
+checkEnv(['DISPATCH_CALLSIGN']);
 if (process.env.DEV_MODE == 'false') {
   checkEnv(['HOPPIE_LOGON']);
+  log.info('Starting scheduler');
   schedule('* * * * *', () => {
     void (async () => {
       await arrivalMessage();
     })();
   });
 } else {
+  log.info('DEV_MODE is on - running once')
   void (async () => {
      await arrivalMessage();
   })();
---
+++ src/log.ts
@@ -0,0 +1,10 @@
+import { Logger, TLogLevelName } from 'tslog'
+
+let min_level = process.env.LOG_LEVEL;
+if (!min_level) {
+  min_level = 'info'
+}
+
+export const log = new Logger({
+  minLevel: min_level as TLogLevelName
+});
