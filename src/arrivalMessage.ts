8407f91285fa83a162bff0c4b4c07e98673e0ffe
Evan Fribourg <evan@dotevan.com>
2023-02-24 18:07:02 -0800

    changing drop message from debug to info

--- src/arrivalMessage.ts
+++ src/arrivalMessage.ts
@@ -66,7 +66,7 @@ const flightShouldReceiveMessage = ({
       log.debug(`Would have dropped message for pilot ${pilot.username} on flight ${callsign}`);
       return true;
     }
-    log.debug(`Dropping message for ${pilot.username}`);
+    log.info(`Dropping message for ${pilot.username}`);
     return false;
 
   }
